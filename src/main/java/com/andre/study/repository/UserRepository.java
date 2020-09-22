e0b3d0e1b28efaf8ebad1914a1675138549e9a4b
andre <hau@yellostory.co.kr>
2020-09-22 14:34:51 +0700

    fix

---
+++ src/main/java/com/andre/study/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.andre.study.repository;
+
+import com.andre.study.enitty.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
