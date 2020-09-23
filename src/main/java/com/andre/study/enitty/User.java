f700b71802c820473248c8023c5bcd0244157872
andre <andreconghau@gmail.com>
2020-09-23 09:20:27 +0700

    adjust config

--- src/main/java/com/andre/study/enitty/User.java
+++ src/main/java/com/andre/study/enitty/User.java
@@ -4,23 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Component
+@Entity
+@Table(name="user")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    @NotNull
+    @Column(name="name", unique = false)
     private String name;
 
+    @NotNull
+    @Column(name="email", unique = false)
     private String email;
 
+    @NotNull
+    @Column(name="role", unique = false)
     private String role;
 
+    @NotNull
+    @Column(name="password", unique = false)
     private String password;
 
 }
